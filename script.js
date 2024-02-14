e03e9935867034276aff516652075182f3a5fd20
SwiftStep <159876441+SwiftStep@users.noreply.github.com>
2024-02-14 11:34:55 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-link').addEventListener('click', function(event) {
+    alert('Danke für Ihr Interesse an XL Möbel. Sie können uns jetzt anrufen!');
+    // Если вы не хотите, чтобы браузер переходил по ссылке после показа уведомления, используйте следующую строку:
+    // event.preventDefault();
+});
